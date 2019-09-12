5bdf152e8b4d9ceda721abda775c5e5f461fe2bb
Yunsi Niu <niuyunsi@gmail.com>
2019-09-12 15:37:44 +0800

    edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <div>Project-5</div>
-      <div>with Docker</div>
-      <div>Heroku</div>
-      <div>nginx</div>
-      <div>node:10</div>
-      <div>and yarn</div>
       <div>ver 0.0.16</div>
     </div>
   );
